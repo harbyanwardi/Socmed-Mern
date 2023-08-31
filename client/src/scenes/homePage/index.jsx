7158d7b0b8ea18cb0da516fa3993b830b8ea8fc1
harbyanwardi <anwardiharby@gmail.com>
2023-08-31 15:35:41 +0700

    1st Commit

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material';
+import Navbar from 'scenes/navbar';
+
+const Homepage = () => {
+    return <Box>
+        <Navbar />
+        </Box>
+}
+
+export default Homepage;
