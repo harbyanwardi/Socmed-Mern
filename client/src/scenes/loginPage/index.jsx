506f061b4650afdc3891d7bdf324e7785a4233c3
harbyanwardi <anwardiharby@gmail.com>
2023-09-01 14:17:49 +0700

    add form page login & register

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,10 +1,10 @@
 import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+import Form from './Form';
 
 
 const LoginPage = () => {
     const theme = useTheme();
     const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-
     return <Box>
         <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
         <Typography
@@ -21,12 +21,12 @@ const LoginPage = () => {
             p="2rem"
             m="2rem auto"
             borderRadius="1.5rem"
-            backgroundColor={them.palette.background.alt}
+            backgroundColor={theme.palette.background.alt}
         >
             <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem"}}>
                 Welcome To The Largest Social Media in Cikarang Planet
             </Typography>
-
+            <Form />
         </Box>
     </Box>
 }
