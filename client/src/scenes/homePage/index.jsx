d6ad3b9eb54f7cba782561ac69ca231fc8dedf4f
harbyanwardi <anwardiharby@gmail.com>
2023-09-04 16:42:48 +0700

    advertise section

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,6 +1,7 @@
 import { Box, useMediaQuery } from '@mui/material';
 import { useSelector } from 'react-redux';
 import Navbar from 'scenes/navbar';
+import AdvertWidget from 'scenes/widgets/AdvertWidget';
 import MyPostWidget from 'scenes/widgets/MyPostWidget';
 import PostsWidget from 'scenes/widgets/PostsWidget';
 import UserWidget from 'scenes/widgets/UserWidget';
@@ -32,7 +33,13 @@ const Homepage = () => {
           <PostsWidget userId={_id} />
         </Box>
 
-        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+        {isNonMobileScreens && 
+          <Box flexBasis="26%" ml="2rem">
+            <AdvertWidget />
+            <Box m="2rem 0">
+
+            </Box>
+          </Box>}
 
       </Box>
     </Box>
